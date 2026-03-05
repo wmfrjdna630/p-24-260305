86f844d4a46cfc197eded872cbd37f81750ae5bb
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-05 12:10:34 +0900

    write

---
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -0,0 +1,40 @@
+package com.back.domain.post.controller;
+
+import com.back.domain.post.entity.Post;
+import com.back.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/posts/write-form")
+    @ResponseBody
+    public String writeForm() {
+
+        return """
+                <form action="/posts/write">
+                  <input type="text" name="title">
+                  <br>
+                  <textarea name="content"></textarea>
+                  <br>
+                  <input type="submit" value="작성">
+                </form>
+                """;
+    }
+
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write(String title, String content) {
+
+        Post post = postService.write(title, content);
+
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
+    }
+
+}
