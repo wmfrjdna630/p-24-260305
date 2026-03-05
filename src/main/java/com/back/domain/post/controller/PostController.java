0a9efd0ae15a50b4e92c2596e31cb1d7cfa6c646
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-05 12:50:10 +0900

    조금 이쁘게 편하게 만들기

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -18,6 +18,34 @@ public class PostController {
     @ResponseBody
     public String writeForm() {
 
+        return getWriteFrom();
+    }
+
+    @PostMapping("/posts/write")
+    @ResponseBody
+    public String write(String title, String content) {
+
+        // 유효성 체크
+        if(title.isBlank()) {
+            return """
+                    <div style="color:red">제목을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteFrom());
+        }
+
+        if(content.isBlank()){
+            return """
+                    <div style="color:red">내용을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteFrom());
+        }
+
+        Post post = postService.write(title, content);
+
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
+    }
+
+    private String getWriteFrom(){
         return """
                 <form method="post" action="/posts/write">
                   <input type="text" name="title">
@@ -27,15 +55,6 @@ public class PostController {
                   <input type="submit" value="작성">
                 </form>
                 """;
-    }
-
-    @PostMapping("/posts/write")
-    @ResponseBody
-    public String write(String title, String content) {
-
-        Post post = postService.write(title, content);
-
-        return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 
 }
