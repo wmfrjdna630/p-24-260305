e5f5b4ed5902ba6d5f3e15b34b4adeddb168b495
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-06 10:54:17 +0900

    error message ì 꾸미기

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -9,7 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,9 +47,13 @@ public class PostController {
 
             String errorMessages = bindingResult.getFieldErrors()
                     .stream()
-                    .map(FieldError::getDefaultMessage)
+                    .map((fieldError) -> fieldError.getField() + "-" + fieldError.getDefaultMessage())
+                    .map((message) -> {
+                        String[] bits = message.split("-"); // [field, 1, errorMessage]
+                        return "<!-- %s --> <li data-error-field=\"%s\">%s</li>".formatted(bits[1], bits[0], bits[2]);
+                    })
                     .sorted()
-                    .collect(Collectors.joining("<br>"));
+                    .collect(Collectors.joining("\n"));
 
             return getWriteForm(errorMessages, form.title, form.content, "title");
         }
@@ -62,7 +65,7 @@ public class PostController {
 
     private String getWriteForm(String errorMessage, String title, String content, String errorFieldName) {
         return """
-                <div style="color:red">%s</div>
+                <ul style="color:red">%s</ul>
                 <form method="post" action="/posts/write">
                   <input type="text" name="title" value="%s" autoFocus>
                   <br>
