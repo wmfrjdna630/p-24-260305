0d6cb44c07f396e57d03d92f9d2d8e4de54f4941
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-06 11:29:57 +0900

    err타임리프 적용을 위한 @ResponseBody 지우고 html파일 만듦

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.stream.Collectors;
 
@@ -22,10 +21,9 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts/write-form")
-    @ResponseBody
     public String writeForm() {
 
-        return getWriteForm("", "", "", "");
+        return "write";
     }
 
     @AllArgsConstructor
@@ -40,7 +38,6 @@ public class PostController {
     }
 
     @PostMapping("/posts/write")
-    @ResponseBody
     public String write(@Valid WriteRequestForm form, BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()) {
@@ -55,34 +52,12 @@ public class PostController {
                     .sorted()
                     .collect(Collectors.joining("\n"));
 
-            return getWriteForm(errorMessages, form.title, form.content, "title");
+            return "write";
         }
 
         Post post = postService.write(form.title, form.content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
-    }
-
-    private String getWriteForm(String errorMessage, String title, String content, String errorFieldName) {
-        return """
-                <ul style="color:red">%s</ul>
-                <form method="post" action="/posts/write">
-                  <input type="text" name="title" value="%s" autoFocus>
-                  <br>
-                  <textarea name="content">%s</textarea>
-                  <br>
-                  <input type="submit" value="작성">
-                </form>
-                
-                <script>
-                    const errorFieldName = "%s";
-                
-                    if(errorFieldName.length > 0) {
-                        const form = document.querySelector("form");
-                        form[errorFieldName].focus();
-                    }
-                </script>
-                """.formatted(errorMessage, title, content, errorFieldName);
     }
 
 }
